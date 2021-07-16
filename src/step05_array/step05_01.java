a4dcc2bd1855fc4b11329e8ceb8e66ccb14a2815
choahkimm <choahk.dev@gmail.com>
2021-07-16 18:15:38 +0900

    0716 test

---
+++ src/step05_array/step05_01.java
@@ -0,0 +1,12 @@
+package step05_array;
+
+public class step05_01
+{
+    public static void main(String[] args)
+            // 10818번. 최소,최대
+            // 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+    {
+
+
+    }
+}
